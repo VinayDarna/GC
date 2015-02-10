8a8f4824c24e21b1fa42b28218a0d00abb79946e
VinayDarna <vinay@olivetech.net>
2015-02-10 18:53:17 +0530

    Added CustomCell for Speaker Details.

---
+++ GatewayConference/MainViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MainViewCell.h
+//  GatewayConference
+//
+//  Created by Olive Tech on 10/02/15.
+//  Copyright (c) 2015 Teja Swaroop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewCell : UITableViewCell
+
+@end
---
+++ GatewayConference/MainViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MainViewCell.m
+//  GatewayConference
+//
+//  Created by Olive Tech on 10/02/15.
+//  Copyright (c) 2015 Teja Swaroop. All rights reserved.
+//
+
+#import "MainViewCell.h"
+
+@implementation MainViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
