cd913ee7e3657feb46edb5c645ec9058d1f8be3e
Teja Swaroop <teja@olivetech.net>
2015-02-19 16:22:56 +0530

    Test Commit

--- GatewayConference/AppDelegate.m
+++ GatewayConference/AppDelegate.m
@@ -17,8 +17,8 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     [AFNetworkActivityIndicatorManager sharedManager].enabled = YES;
     
     
