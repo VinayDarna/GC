a733976137d2451852ec8d84d2c0b4d2a8036c02
VinayDarna <vinay@olivetech.net>
2015-03-17 12:29:30 +0530

    AddedPOIClass

---
+++ GatewayConference/Viewcontroller/HashTag/HashTagViewController.h
@@ -0,0 +1,13 @@
+//
+//  HashTagViewController.h
+//  GatewayConference
+//
+//  Created by Olive Tech on 17/03/15.
+//  Copyright (c) 2015 Teja Swaroop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HashTagViewController : UIViewController
+
+@end
---
+++ GatewayConference/Viewcontroller/RealEyes/RealEyesViewController.h
@@ -0,0 +1,13 @@
+//
+//  RealEyesViewController.h
+//  GatewayConference
+//
+//  Created by Olive Tech on 17/03/15.
+//  Copyright (c) 2015 Teja Swaroop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RealEyesViewController : UIViewController
+
+@end
